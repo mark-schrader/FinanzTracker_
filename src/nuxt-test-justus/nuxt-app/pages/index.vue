fdf0ef7c926d03e5f19415a9ef3504b28b72bbb1
Mueller Justus <justus.mueller@stud.htw-dresden.de>
2025-06-08 23:27:57 +0200

    add css settings to nuxt-test-justus

--- src/nuxt-test-justus/nuxt-app/pages/index.vue
+++ src/nuxt-test-justus/nuxt-app/pages/index.vue
@@ -1,4 +1,9 @@
 <template>
     <h1>Was geht!</h1>
     <p>erster Nuxt-Test</p>
+    <img src="/public/pleitegeier_logo.png" alt="Logo Pleitegeier" />
 </template>
+
+<style>
+@import url("animate.css");
+</style>
