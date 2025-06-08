86da345ee8d0d7404938efa77659cb99bf5de2da
Vogt Erik <s87069@stud.htw-dresden.de>
2025-06-08 16:39:59 +0200

    t

--- src/nuxt-test-erik/nuxt1/server/api/user/create.ts
+++ src/nuxt-test-erik/nuxt1/server/api/user/create.ts
@@ -1,19 +1,26 @@
 async function submit() {
   console.log("Formular wurde abgeschickt")
 
-  const response = await $fetch('/api/user/create', {
-    method: 'POST',
-    body: {
-      firstname: firstname.value,
-      lastname: lastname.value,
-      university: university.value,
-      birthdate: new Date(birthdate.value).toISOString(),
-      startamount: Math.round(startamount.value * 100),
-      username: username.value,
-      email: email.value,
-      password: password.value
-    }
-  })
+  const data = {
+    firstname: firstname.value,
+    lastname: lastname.value,
+    university: university.value,
+    birthdate: new Date(birthdate.value).toISOString(),
+    startamount: Math.round(startamount.value * 100),
+    username: username.value,
+    email: email.value,
+    password: password.value
+  }
 
-  console.log('User angelegt:', response)
+  console.log('Wird gesendet:', data)
+
+  try {
+    const response = await $fetch('/api/user/create', {
+      method: 'POST',
+      body: data
+    })
+    console.log('User erfolgreich angelegt:', response)
+  } catch (err) {
+    console.error('Fehler beim Speichern:', err)
+  }
 }
