6479b7c2d5fce12c434e3a73a7b27fe1df7f22bd
Vogt Erik <s87069@stud.htw-dresden.de>
2025-06-08 16:32:42 +0200

    t

--- src/nuxt-test-erik/nuxt1/server/api/user/create.ts
+++ src/nuxt-test-erik/nuxt1/server/api/user/create.ts
@@ -1,22 +1,22 @@
+async function submit() {
+  console.log("Formular wurde abgeschickt")
 
-import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
-
-export default defineEventHandler(async (event) => {
-  const body = await readBody(event)
-
-  const user = await prisma.user.create({
-    data: {
-      firstname: body.firstname,
-      lastname: body.lastname,
-      university: body.university,
-      birthdate: new Date(body.birthdate),
-      startamount: body.startamount,
-      username: body.username,
-      email: body.email,
-      password: body.password 
-    }
-  })
-
-  return user
-})
+  try {
+    const response = await $fetch('/api/user/create', {
+      method: 'POST',
+      body: {
+        firstname: firstname.value,
+        lastname: lastname.value,
+        university: university.value,
+        birthdate: new Date(birthdate.value).toISOString(),
+        startamount: Math.round(startamount.value * 100),
+        username: username.value,
+        email: email.value,
+        password: password.value
+      }
+    })
+    console.log("Antwort von API:", response)
+  } catch (error) {
+    console.error("Fehler beim Senden:", error)
+  }
+}
