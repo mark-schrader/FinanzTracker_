5d3f8522a037255082cb6b625be13a473198c4e4
Müller Justus <s87089@stud.htw-dresden.de>
2025-06-11 17:24:59 +0200

    add index nuxt-justus-laptop

--- src/nuxt-test-justus/nuxt-app-laptop/pages/index.vue
+++ src/nuxt-test-justus/nuxt-app-laptop/pages/index.vue
@@ -1,6 +1,6 @@
 <template>
      <Header />
-     <header />
+     <Header2 />
 <h1>Wilkommen bei Pleitegeier</h1>
 <p>Wir machen bei deienen Finanzen Klarschiff und veranschaulichendir deine Geldbewegungen im Altag.</p>
 
@@ -12,4 +12,5 @@
 
 <script setup>
 import Header from '~/components/Header.vue'
+import Header2 from '~/components/header copy.vue'
 </script>
