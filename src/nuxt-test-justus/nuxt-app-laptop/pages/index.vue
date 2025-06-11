dda0aad9c06b0a1d4f3814307b87dcb8b2ed303b
Müller Justus <s87089@stud.htw-dresden.de>
2025-06-11 16:29:03 +0200

    add css nuxt_jutsu_laptop

---
+++ src/nuxt-test-justus/nuxt-app-laptop/pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+     <header />
+<h1>Wilkommen bei Pleitegeier</h1>
+<p>Wir machen bei deienen Finanzen Klarschiff und veranschaulichendir deine Geldbewegungen im Altag.</p>
+
+</template>
+
+<style>
+@import url("animate.css");
+</style>
+
+<script setup>
+import header from '~/components/header copy.vue'
+</script>
