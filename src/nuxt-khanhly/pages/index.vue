d19d7cbd53e970072ecccec6732838b365ff6c52
Khanh Ly La <khanhly.la@stud.htw-dresden.de>
2025-06-12 15:59:26 +0200

    nuxt-test

---
+++ src/nuxt-khanhly/pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+     <Header2 />
+<h1>Wilkommen bei Pleitegeier</h1>
+<p>Wir machen bei deienen Finanzen Klarschiff und veranschaulichendir deine Geldbewegungen im Altag.</p>
+
+</template>
+
+<style>
+@import url("animate.css");
+</style>
+
+<script setup>
+import Header from '~/components/header copy.vue'
+import Header2 from '~/components/header.vue'
+</script>
