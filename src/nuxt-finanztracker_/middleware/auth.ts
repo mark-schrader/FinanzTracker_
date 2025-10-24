608f7984fce971cadc1d01eb3820647587bc7461
Vogt Erik <s87069@stud.htw-dresden.de>
2025-10-24 15:35:25 +0200

    Wechsel zu usesupabseUSer

--- src/nuxt-finanztracker_/middleware/auth.ts
+++ src/nuxt-finanztracker_/middleware/auth.ts
@@ -1,20 +1,14 @@
 import { defineNuxtRouteMiddleware, navigateTo } from 'nuxt/app'
 
 export default defineNuxtRouteMiddleware((to, from) => {
-  // Token aus localStorage holen (nur im Client)
-  // Token aus localStorage holen (nur im Client)
-  const token = process.client ? localStorage.getItem('auth_token') : null
-  // Dashboard-Routen schützen
-  if (to.path.startsWith('/dashboard') && !token) {
-    // Optional: Info-Message anzeigen (z.B. mit useNuxtApp().$toast)
-    // useNuxtApp().$toast?.error('Bitte zuerst einloggen!')
+  
+const user = useSupabaseUser()
+  //const token = process.client ? localStorage.getItem('auth_token') : null
 
-    // Weiterleitung zur Startseite
+  if (to.path.startsWith('/dashboard') && !user.value) {
+
     return navigateTo('/')
   }
 
-  // Optional: Weitere geschützte Routen hinzufügen
-  // if (to.path.startsWith('/profile') && !token) {
-  //   return navigateTo('/')
-  // }
+  
 })
