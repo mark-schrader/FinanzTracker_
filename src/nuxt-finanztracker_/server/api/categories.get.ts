cbdf1ff1ccd03661b59618415e47ef2d4f9c29fe
mark-schrader <mark_schrader@outlook.com>
2025-11-13 23:06:28 +0100

    Reperatur des Merges

--- src/nuxt-finanztracker_/server/api/categories.get.ts
+++
@@ -1,6 +0,0 @@
-import CategoryController from '../controllers/CategoryController'
-
-export default defineEventHandler(async (event) => {
-  const userId = Number(getQuery(event).user_id) || 1 // später aus Auth
-  return await CategoryController.getAll(userId)
-})
