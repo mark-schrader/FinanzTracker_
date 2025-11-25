deef5bb3c7f41aebef47ac4aea2d1b103dbc49b0
mark-schrader <mark_schrader@outlook.com>
2025-11-25 14:48:38 +0100

    Expenses new Structur

---
+++ src/nuxt-finanztracker_/server/api/expenses/[id].ts
@@ -0,0 +1,34 @@
+import ExpenseService from '../../application/ExpenseService'
+
+export default defineEventHandler(async (event) => {
+  const method = getMethod(event)
+  const id = getRouterParam(event, 'id')
+  const userId = getQuery(event).userId // GET /api/expenses?userId=123
+
+  try {
+    switch (method) {
+      case 'GET':
+        // Anzeige einer einzelnen Ausgabe
+          // GET /api/expenses/5
+          return await ExpenseService.getExpenseById(Number(id))
+
+      case 'PUT': { // Verändern einer Ausgabe
+        // PUT /api/expenses/5
+        if (!id) throw createError({ statusCode: 400, message: 'Missing ID' })
+        const body = await readBody(event)
+        return await ExpenseService.updateExpense(Number(id), body)
+      }
+
+      case 'DELETE': // Löschen einer Ausgabe
+        // DELETE /api/expenses/5
+        if (!id) throw createError({ statusCode: 400, message: 'Missing ID' })
+        return await ExpenseService.deleteExpense(Number(id))
+
+      default:
+        throw createError({ statusCode: 405, message: `Method ${method} not allowed` })
+    }
+  } catch (error: any) {
+    console.error('[expenses API error]', error)
+    throw createError({ statusCode: 500, message: error.message || 'Server error' })
+  }
+})
