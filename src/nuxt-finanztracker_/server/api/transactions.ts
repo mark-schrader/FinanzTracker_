0f8a916aaf925b3c500ee388428a67b25ea56df9
Vogt Erik <s87069@stud.htw-dresden.de>
2025-12-03 16:24:12 +0100

    supabaseUser für Datensicherheit eingefügt

--- src/nuxt-finanztracker_/server/api/transactions.ts
+++ src/nuxt-finanztracker_/server/api/transactions.ts
@@ -1,13 +1,33 @@
+
+import { serverSupabaseUser } from '#supabase/server'
+import { PrismaClient } from '@prisma/client'
 import TransactionService from '../application/TransactionService'
+
+
+const prisma = new PrismaClient()
 
 export default defineEventHandler(async (event) => {
   const method = getMethod(event)
-  const userId = getQuery(event).userId // /api/transactions?userId=1
+  
+  const supabaseUser = serverSupabaseUser(event)
+
+  if (!supabaseUser) {
+    throw createError({ statusCode: 401, message: 'Nicht Authorisiert!' })
+  }
+
+  const prismaUser = await prisma.user.findUnique({
+    where: { supabaseid: supabaseUser.id }
+  })
+  if (!prismaUser) {
+    throw createError({ statusCode: 401, message: 'Benutzer nicht gefunden!' })
+  }
+
+  const userId = prismaUser.userid
 
   try {
     switch (method) {
       case 'GET': // GET /api/transactions?userId=123
-        if (!userId) throw createError({ statusCode: 400, message: 'Missing userId' }) // Anzeige aller Transaktionen eines Benutzers
+        
         return await TransactionService.getAllTransactions(Number(userId))
 
       default:
