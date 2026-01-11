eae3aff2f9b61527759e7627bf8fe1eeb9d8fad2
mark-schrader <mark_schrader@outlook.com>
2026-01-11 20:57:58 +0100

    funktionierendes Login und Session

--- src/nuxt-finanztracker_/test/e2e/dashboard/dashboard.spec.ts
+++ src/nuxt-finanztracker_/test/e2e/dashboard/dashboard.spec.ts
@@ -1,6 +1,10 @@
 import { test, expect } from '@playwright/test';
 
-test('Dashboard ist da', async ({ page }) => {
-  await page.goto('http://localhost:3000/dashboard');
-  await expect(page.getByText('Dashboard')).toBeVisible();
+test.use({
+  storageState: 'auth.json'
+});
+
+test('dashboard ist da', async ({ page }) => {
+  await page.goto('http://localhost:3000/dashboard/64');
+  await expect(page.getByRole('heading', { name: 'Aktueller Kontostand: 0,00 €' })).toBeVisible();
 });
--- src/nuxt-finanztracker_/test/e2e/setup/auth.setup.ts
+++ src/nuxt-finanztracker_/test/e2e/setup/auth.setup.ts
@@ -2,16 +2,14 @@ import { test, expect } from '@playwright/test';
 
 test('login', async ({ page }) => {
   await page.goto('http://localhost:3000/');
-  await page.getByRole('banner').getByRole('button').click();
+  await expect(page.getByRole('heading', { name: 'WILLKOMMEN IN PLEITEGEIER!' })).toBeVisible();
   await page.getByRole('button', { name: 'Sign In' }).click();
   await page.getByRole('textbox', { name: 'Enter Email' }).click();
   await page.getByRole('textbox', { name: 'Enter Email' }).fill('TestE2E@test.de');
   await page.getByRole('textbox', { name: 'Enter Password' }).click();
   await page.getByRole('textbox', { name: 'Enter Password' }).fill('TestTestE2E');
   await page.getByRole('button', { name: 'Login' }).click();
-
-  // WICHTIG: warten, bis Login wirklich fertig ist
-  await expect(page.getByRole('link', { name: 'DASHBOARD' })).toBeVisible();
+  await expect(page.getByRole('heading', { name: 'Dashboard' })).toBeVisible();
 
   // Session speichern
   await page.context().storageState({ path: 'auth.json' });
