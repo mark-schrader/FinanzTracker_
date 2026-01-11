d36e5e0673b5a08f0a588cfb302623afee806658
mark-schrader <mark_schrader@outlook.com>
2026-01-11 21:20:43 +0100

    github action test

--- src/nuxt-finanztracker_/test/e2e/dashboard/dashboard.spec.ts
+++ src/nuxt-finanztracker_/test/e2e/dashboard/dashboard.spec.ts
@@ -4,6 +4,7 @@ test.use({
   storageState: 'auth.json'
 });
 
+// Testet, ob das Dashboard geladen wird
 test('dashboard ist da', async ({ page }) => {
   await page.goto('http://localhost:3000/dashboard/64');
   await expect(page.getByRole('heading', { name: 'Aktueller Kontostand: 0,00 €' })).toBeVisible();
